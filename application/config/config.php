b8a19d303604d1fd7fcfc0975abaf59c02b9b38e
KalebMolewe23 <molewekaleb33@gmail.com>
2023-06-26 08:44:19 +0700

    http => https

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://localhost/vaksinasi-bitung/';
-$config['base_url'] = 'http://vaksinasicovidbitung.online/';
+$config['base_url'] = 'https://vaksinasicovidbitung.online/';
 
 /*
 |--------------------------------------------------------------------------
